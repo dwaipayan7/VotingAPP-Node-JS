09fc6ce3186c09e7b1013395966f4bde5bb62c05
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-10-03 17:19:58 +0530

    Successfully created token and signup

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -38,6 +40,39 @@ const userSchema = new mongoose.Schema({
         default: false
     }
 });
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    try {
+        
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+
+        user.password = hashedPassword;
+        next();
+
+    } catch (error) {
+        return next(error);
+    }
+
+    userSchema.methods.comparePassword = async function(candidatePassword){
+        try {
+            
+            const isMatch = await bcrypt.compare(candidatePassword, this.password);
+            return isMatch;
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+})
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-require('dotenv').config()
+require('dotenv').config(); // Load environment variables
+const connectDB = require('./config/db')
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // Middleware for parsing JSON
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`Server Running in ${PORT}`);
-})
+connectDB();
 
+// Import user routes
+const userRoutes = require('./routes/user.routes');
+
+// Use the imported routes
+app.use('/user', userRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
